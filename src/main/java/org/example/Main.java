30ce5aa3f213fcc0c3761d6f966a864bee19f9ec
3to5thpower <nisi3genki@gmail.com>
2022-06-13 20:46:35 +0900

    :tada: initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
