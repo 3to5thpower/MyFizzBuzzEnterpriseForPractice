abbfdb9fb970ae4fd4c954405869b05ea633e034
3to5thpower <nisi3genki@gmail.com>
2022-06-16 14:05:44 +0900

    :art: move FizzBuzz Logic

---
+++ src/main/java/com/seriouscompany/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,17 @@
+package com.seriouscompany.fizzbuzz;
+
+public class FizzBuzz {
+    public void fizzbuzz(int number) {
+        for(int i = 0; i < 100; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
--- src/main/java/com/seriouscompany/fizzbuzz/Main.java
+++ src/main/java/com/seriouscompany/fizzbuzz/Main.java
@@ -2,16 +2,7 @@ package com.seriouscompany.fizzbuzz;
 
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i < 100; i++) {
-            if(i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if(i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if(i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-        }
+        FizzBuzz fb = new FizzBuzz();
+        fb.fizzbuzz(100);
     }
 }
